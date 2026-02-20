ee2e964b76ba1adcd20b3a08a3668e345ffffde4
manacap-9e-ay2526 <manacap.jy@obmontessori.edu.ph>
2026-02-20 11:26:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// Function for Discount button
+function buttonDiscount () {
+    let origBudget = document.getElementById(budgetInput).value;
+    // const discountValue = .2;
+    let discountedPrice = Number(origBudget * 0.2);
+
+    document.getElementById().innerHTML = '
+        <div class="container">
+    
+        </div>
+    '
+}
