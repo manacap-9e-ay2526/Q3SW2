fdb084450de13df5a28bab97924c52744e89b359
manacap-9e-ay2526 <manacap.jy@obmontessori.edu.ph>
2026-02-27 10:34:58 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-// Function for Discount button
-function buttonDiscount () {
-    let origBudget = document.getElementById(budgetInput).value;
-    // const discountValue = .2;
-    let discountedPrice = Number(origBudget * 0.2);
+function getDiscount(){
 
-    document.getElementById().innerHTML = '
-        <div class="container">
-    
+let origBudget= discount.getElement(what)
+let disValue=Number(.2*origBudget);
+
+document.getElementById("discbox").innerHTML
+
+}
+
+function getDiscount(){
+    let budg = document.getElementById("cost").value;
+    let disva = Number(budg * 0.2);
+
+    document.getElementById("disdisbut").innerHTML = `
+        <div class="container bg-secondary">
+            <h2 class="text-white">Recommended Brands:</h2>
+            <button class="btn btn-success"> Acer </button>
+            <button class="btn btn-success"> Asus </button>
+            <button class="btn btn-success"> Dell </button>
+            <button class="btn btn-success"> Lenovo </button>
+            <button class="btn btn-success"> MSI  </button>
+            <p>Eligible Discount:<span class="bg-danger">${disva}</span></p>
         </div>
-    '
+    `
+}
+
+function getLoction(){
+    document.getElementById("Storeloc").innerHTML = `
+        <div class="container bg-secondary">
+                    <img src="PC Express LOGO.webp" width="200px">
+                    <hr>
+                    <p>
+                        Quezon City: ROG Concept Store SM North<br>
+                        Mandaluyong City: PC Express - SM Megamall<br>
+                        Caloocan City: SM Caloocan located at CZ-13 3F SM<br>
+                        Manila City: SM San Lazaro! located at 3/F CYBERZONE SM SAN LAZARO, FELIX HUERTAS STREET CORNER LACSON AVENUE (C-2 ROAD), SANTA CRUZ<br>
+                    </p>
+        </div>
+    `
 }
